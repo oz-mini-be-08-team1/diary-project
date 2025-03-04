3f144d2adeadcc0215858c4d083a947db7337484
shinezeee <h46998600@gmail.com>
2025-03-04 17:21:57 +0900

    ✨ User 모델 생성

--- diary/models.py
+++ diary/models.py
@@ -1,3 +1,10 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 # Create your models here.
+
+# 사용자
+class User(AbstractUser):
+    email = models.EmailField(unique=True)
+    name = models.CharField(max_length=255)
+    nickname = models.CharField(max_length=50)
