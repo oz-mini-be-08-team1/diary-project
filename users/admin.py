c8aa7182e57a7d6512accb4d0276d39e96d6b4ad
shinezeee <h46998600@gmail.com>
2025-03-05 17:06:02 +0900

    ✏️ Black,isort 실행

--- users/admin.py
+++ users/admin.py
@@ -6,21 +6,33 @@ from django.contrib.auth.admin import UserAdmin
 
 User = get_user_model()
 
+
 @admin.register(User)
 class UserAdmin(UserAdmin):
-    list_display = ('name', 'nickname','email', 'last_login', )
-    list_filter = ('is_staff','is_active')
-    search_fields = ('email','nickname','phone_number')
-
+    list_display = (
+        "name",
+        "nickname",
+        "email",
+        "last_login",
+    )
+    list_filter = ("is_staff", "is_active")
+    search_fields = ("email", "nickname", "phone_number")
 
     fieldsets = (
-        ("User Info",{'fields':('nickname','email','phone_number')}), # 사용자의 기본정보 표시
-        ("Permissions",{'fields':('is_staff','is_active','is_superuser')}),  # 권한 관련 필드 표시
+        (
+            "User Info",
+            {"fields": ("nickname", "email", "phone_number")},
+        ),  # 사용자의 기본정보 표시
+        (
+            "Permissions",
+            {"fields": ("is_staff", "is_active", "is_superuser")},
+        ),  # 권한 관련 필드 표시
     )
 
-    readonly_fields = ('last_login','is_superuser') # 읽기 전용 필드
+    readonly_fields = ("last_login", "is_superuser")  # 읽기 전용 필드
 
-    add_fieldsets = ("User Info",
+    add_fieldsets = (
+        "User Info",
         {
             "classes": ("wide",),
             "fields": ("username", "usable_password", "password1", "password2"),
