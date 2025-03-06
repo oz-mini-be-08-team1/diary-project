19c58b0716d13c727230ecb57de36bb8d10edda7
shinezeee <h46998600@gmail.com>
2025-03-06 15:34:48 +0900

    ✨ Diary urls 연결

--- diary/views.py
+++ diary/views.py
@@ -18,25 +18,21 @@ class DiaryCreateView(generics.CreateAPIView):
         #일기 작성 시 로그인한 사용자정보 추가
         serializer.save(author=self.request.user)# 일기 쓴사람 author로 정의
 
-# 일기 조회
-class DiarylistView(generics.ListAPIView):
-    queryset = Diary.objects.all().order_by('-created_at') # 최신순으로 정렬
-    serializer_class = DiarySerializer
-    permission_classes = [IsAuthenticated]
-
-    def get_queryset(self):
-        return Diary.objects.all().order_by('-created_at')
-
 class DiaryUpdateView(generics.RetrieveUpdateAPIView):
     queryset = Diary.objects.all()
     serializer_class = DiarySerializer
     permission_classes = [IsAuthenticated]
 
-    def get_queryset(self):
+    def get_object(self):
         diary = super().get_object()
         if diary.author != self.request.user:
-            raise PermissionDenied("본인 일기만 수정가능")
+            raise PermissionDenied("본인 일기만 수정 가능")
         return diary
+
+class DiaryDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Diary.objects.all()
+    serializer_class = DiarySerializer
+    permission_classes = [IsAuthenticated]
 
 class DiaryDeleteView(generics.DestroyAPIView):
     queryset = Diary.objects.all()
@@ -46,25 +42,18 @@ class DiaryDeleteView(generics.DestroyAPIView):
     def get_object(self):
         diary = super().get_object()
         if diary.author != self.request.user:
-            raise PermissionDenied("본인 일기만 삭제가능")
+            raise PermissionDenied("본인 일기만 삭제 가능")
         return diary
 
 class DiaryListView(generics.ListAPIView):
-    queryset = Diary.objects.all()
     serializer_class = DiarySerializer
     permission_classes = [IsAuthenticated]
 
-
     def get_queryset(self):
-        queryset = Diary.objects.all()
-        sort = self.request.GET.get('sort','new')
-        q = self.request.GET.get('q')
-        date = self.request.GET.get('date',"")
-
-        if sort == 'old':
-            queryset = queryset.order_by('created_at')
-        else:
-            queryset = queryset.order_by('-created_at')
+        queryset = Diary.objects.filter(author=self.request.user) # 특정 사용자의 리스트
+        q = self.request.GET.get('q') # 제목,내용에서 검색
+        date = self.request.GET.get('date',"") # 날짜로 검색
+        sort = self.request.GET.get("sort", "new")  # 정렬 기준 (기본값: 최신순)
 
         if q:
             queryset = queryset.filter(   # 검색
@@ -75,6 +64,11 @@ class DiaryListView(generics.ListAPIView):
             parsed_date = parse_date(date)
             if parsed_date:
                 queryset = queryset.filter(created_at__date=parsed_date)
+
+        if sort == "old":
+            queryset = queryset.order_by("created_at")  # 오래된 순
+        else:
+            queryset = queryset.order_by("-created_at")  # 최신순 (기본값)
 
         return queryset
 
