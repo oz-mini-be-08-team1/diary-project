5dbfa3f138c53ab3c1de561c5c874bf22342991e
shinezeee <h46998600@gmail.com>
2025-03-06 10:29:19 +0900

    ✨레지스터 시리얼라이저 추가, 잘못된필드 수정

--- users/serializers.py
+++ users/serializers.py
@@ -1,8 +1,28 @@
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
 from rest_framework import serializers
 
+User = get_user_model()
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ["id", "username", "email", "first_name", "last_name"]
+        fields = ["email", "name","nickname","password"]
+
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ["email", "name","nickname","password"]
+
+        def create(self, validated_data):
+            user = User(
+                email=validated_data["email"],
+                name=validated_data["name"],
+                nickname=validated_data["nickname"],
+            )
+
+            user.set_password(validated_data["password"]) # 비밀번호 암호화
+            user.save()
+            return user
